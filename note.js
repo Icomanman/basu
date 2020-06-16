8dc998a11e651f04f2054b4793af3e72845f14ef
icomanman <fred_barabs@yahoo.com>
2020-06-16 12:44:50 +0800

    updated note.js

--- note.js
+++ note.js
@@ -1,39 +1,36 @@
 const fs = require("fs");
 
 const proj_dir = "C:\\Users\\GiulianoDiEnrico\\Desktop\\SkyCivEng";
-const pack_dir = `${proj_dir}\\design-connections-calculations`;
+const pack_dir = `${proj_dir}\\design-connection-calculations`;
 const mod_dir = `${pack_dir}\\Calculations\\AISC\\WtoW\\modules`;
-const rel_path = `${mod_dir}\\block_shear\\`;
+// const mod_dir = `${pack_dir}\\Calculations\\AISC\\Brace\\`;
 
-const file = "block_shear_AISC.js";
+const rel_path = `${mod_dir}\\report_modules\\`;
+// const rel_path = `${mod_dir}\\block_shear\\`;
+
+// const file = "block_shear_AISC.js";
+const file = "common_rep.js";
 
 let i = 0;
 const main = () => {
-
     let dt = new Date();
     // let tm = dt.getTime();
 
-    let log = fs.createWriteStream(rel_path, { flags: 'a' });
-
+    let log = fs.createWriteStream(`${rel_path}${file}`, { flags: 'a' });
     if (file.indexOf(".js") > -1) {
         // for javascript 
         var data = `// TODO: save file; commit your changes`;
-    }
-    else if (file.indexOf(".py") > -1) {
+    } else if (file.indexOf(".py") > -1) {
         // for python
         var data = `# REM: save file; commit changes`;
-    }
-    else {
+    } else {
         var data = "just some text file."
-    }
-
+    };
     log.write(`\n${data}\n`);
     log.end();
-
     console.log("Successfully Written to File.");
     console.log(`${dt.toLocaleTimeString()}: ${i}`);
     i++;
-
 }
 
 const runnr = initFunc => initFunc();
