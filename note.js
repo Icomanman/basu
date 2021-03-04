d7e7c9c1ef7f0cec97805f6ad27e22d44c44e9c0
icomanman <fred_barabs@yahoo.com>
2021-03-04 15:27:25 +0800

    succesfully update single plate right side weld; updated fixture switching bug.

--- note.js
+++ note.js
@@ -3,13 +3,21 @@ const fs = require("fs");
 const proj_dir = "C:\\Users\\GiulianoDiEnrico\\Desktop\\SkyCivEng";
 const pack_dir = `${proj_dir}\\design-connection-calculations`;
 const mod_dir = `${pack_dir}\\Calculations\\AISC\\WtoW\\modules`;
+const ui_dir = `${proj_dir}\\design-connection-ui-2020\\js\\`;
 // const mod_dir = `${pack_dir}\\Calculations\\AISC\\Brace\\`;
 
-const rel_path = `${mod_dir}\\report_modules\\`;
+// const rel_path = `${mod_dir}\\report_modules\\`;
 // const rel_path = `${mod_dir}\\block_shear\\`;
 
 // const file = "block_shear_AISC.js";
-const file = "common_rep.js";
+// const file = "common_rep.js";
+
+const rel_path = `${ui_dir}\\mod\\`;
+// const rel_path = `${ui_dir}\\mod\\graphics\\renderer`;
+// const file = "graphics_extension.mjs";
+// const file = "fixtures_graphics\\welds.mjs";
+// const file = "part.js";
+const file = "graphics_events.mjs";
 
 let i = 0;
 const main = () => {
@@ -17,7 +25,7 @@ const main = () => {
     // let tm = dt.getTime();
 
     let log = fs.createWriteStream(`${rel_path}${file}`, { flags: 'a' });
-    if (file.indexOf(".js") > -1) {
+    if (file.match("js")) {
         // for javascript 
         var data = `// TODO: save file; commit your changes`;
     } else if (file.indexOf(".py") > -1) {
